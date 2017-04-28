2bf6eb495e185c37e51e3c28be90884a3e54b8cd
natxoraga22 <natxoraga22@gmail.com>
2017-04-28 20:40:41 +0200

    Projectile explosions
    When a projectile hits an enemy (or the player) a little effect is
    shown indicating the hit.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,14 +3,29 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-	public int damage = 1;
+	public GameObject projectileHitPrefab;
 
+	public int damage = 1;
+	public float explosionDuration = 0.1f;
+	public bool isShotUpwards = true;
+
+	private SpriteRenderer spriteRenderer;
+
+
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
 
 	public int GetDamage () {
 		return damage;
 	}
 
 	public void Hit () {
+		// Create an explosion
+		Vector3 projectileHitPosition = this.transform.position + new Vector3 (0f, (isShotUpwards ? 1f : -1f) * spriteRenderer.bounds.size.y / 2f, 0f);
+		GameObject projectileHit = (GameObject) Instantiate (projectileHitPrefab, projectileHitPosition, Quaternion.identity);
+		Destroy (projectileHit, explosionDuration);
+		// Destroy the projectile
 		Destroy (this.gameObject);
 	}
 
